572623f88a138875e3fe23515b8e204378bff605
Naughty Doge <naughty-doge@protonmail.com>
2018-04-01 23:47:05 +0700

    Rework find()

--- src/AdapterBase.js
+++ src/AdapterBase.js
@@ -1,19 +1,3 @@
-function paginate(itemsPerPage, skip, limit) {
-  let firstPage = Math.ceil((skip + 0.1) / itemsPerPage) || 1
-  let pageCount = Math.ceil(limit / itemsPerPage)
-  let pages = []
-
-  for (let i = firstPage; pages.length < pageCount; i++) {
-    pages.push(i)
-  }
-
-  return {
-    pages,
-    skipOnFirstPage: skip % itemsPerPage,
-  }
-}
-
-
 class AdapterBase {
   static SUPPORTED_TYPES = []
 
@@ -25,34 +9,50 @@ class AdapterBase {
     return stream
   }
 
-  async _findByPages(query, pages) {
+  _paginate(request, itemsPerPage = this.constructor.ITEMS_PER_PAGE) {
+    let {
+      skip = 0,
+      limit = itemsPerPage,
+    } = request
+    let firstPage = Math.ceil((skip + 0.1) / itemsPerPage) || 1
+    let pageCount = Math.ceil(limit / itemsPerPage)
+    let pages = []
+
+    for (let i = firstPage; pages.length < pageCount; i++) {
+      pages.push(i)
+    }
+
+    return {
+      pages, skip, limit,
+      skipOnFirstPage: skip % itemsPerPage,
+    }
+  }
+
+  async _find(query, pagination) {
+    let {
+      pages,
+      limit = Infinity,
+      skipOnFirstPage = 0,
+    } = pagination
+
     let requests = pages.map((page) => {
       return this._findByPage(query, page)
     })
+
     let results = await Promise.all(requests)
-    return [].concat(...results)
+    results = [].concat(...results)
+    return results.slice(skipOnFirstPage, skipOnFirstPage + limit)
   }
 
   async find(request) {
-    let { SUPPORTED_TYPES, ITEMS_PER_PAGE } = this.constructor
-    let { query, skip, limit } = request
-    let results
+    let { SUPPORTED_TYPES } = this.constructor
 
-    if (!SUPPORTED_TYPES.includes(query.type)) {
-      throw new Error(`Content type ${query.type} is not supported`)
+    if (!SUPPORTED_TYPES.includes(request.query.type)) {
+      throw new Error(`Content type ${request.query.type} is not supported`)
     }
 
-    skip = skip || 0
-    limit = (!limit || limit === Infinity) ? ITEMS_PER_PAGE : limit
-
-    if (this._findByPages) {
-      let { pages, skipOnFirstPage } = paginate(ITEMS_PER_PAGE, skip, limit)
-      results = await this._findByPages(query, pages)
-      results = results.slice(skipOnFirstPage, skipOnFirstPage + limit)
-    } else {
-      results = await this._findBySkipLimit(query, skip, limit)
-    }
-
+    let pagination = this._paginate(request)
+    let results = await this._find(request.query, pagination)
     return results.map((item) => this._normalizeItemResult(item))
   }
 
