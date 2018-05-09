7ca2069039e042eb4ae2ef7a3738e6ddfdbfe51c
Naughty Doge <naughty-doge@protonmail.com>
2018-05-09 11:41:27 +0700

    Change the sort property prefix to make Discover pagination work

--- src/PornClient.js
+++ src/PornClient.js
@@ -12,7 +12,7 @@ import Chaturbate from './adapters/Chaturbate'
 
 
 const ID = 'porn_id'
-const SORT_PROP_PREFIX = 'porn.'
+const SORT_PROP_PREFIX = 'popularities.porn.'
 const MAX_ADAPTERS_PER_REQUEST = 1
 const ADAPTERS = [PornHub, RedTube, YouPorn, SpankWire, PornCom, Chaturbate]
 const SORTS = ADAPTERS.map(({ name, DISPLAY_NAME, SUPPORTED_TYPES }) => ({
